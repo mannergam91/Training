b5225a443e3ba33530ff968e2b005d2d6721228f
MMakhov <mahovme@gmail.com>
2020-01-22 16:32:35 +0300

    11

---
+++ src/Test5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test5 {
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int x = scan.nextInt();
+
+        System.out.println(x + " x 1 = " + (x*1));
+        System.out.println(x + " x 2 = " + (x*2));
+        System.out.println(x + " x 3 = " + (x*3));
+        System.out.println(x + " x 4 = " + (x*4));
+        System.out.println(x + " x 5 = " + (x*5));
+        System.out.println(x + " x 6 = " + (x*6));
+        System.out.println(x + " x 7 = " + (x*7));
+        System.out.println(x + " x 8 = " + (x*8));
+        System.out.println(x + " x 9 = " + (x*9));
+        System.out.println(x + " x 10 = " + (x*10));
+    }
+}
