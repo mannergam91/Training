609fd0a6a820a698c32f6c722d2423ee00cb3f6f
MMakhov <mahovme@gmail.com>
2020-01-22 17:05:26 +0300

    11

---
+++ src/Test11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Test11 {
+
+    public static void main(String args[]) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст который будет помещен в строковую переменную s");
+        String s = scanner.nextLine();
+        System.out.println("Текст помещенный в строковую переменную: " + "'" + s + "'");
+    }
+
+}
