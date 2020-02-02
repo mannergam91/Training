6bddeaf985f8aa0647173c346fe68afc2edfea6b
MMakhov <mahovme@gmail.com>
2020-02-02 16:45:03 +0300

    11

---
+++ src/Test15.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Test15 {
+    public static void main(String[] args) {
+        int[] mas = {11, 3, 14, 16, 7};
+
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
