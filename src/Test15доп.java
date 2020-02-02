44b20abb82695d2384e7a4fde530e778761fc162
MMakhov <mahovme@gmail.com>
2020-02-02 16:50:48 +0300

    11

---
+++ src/Test15доп.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class Test15доп {
+
+    public static void main(String[] args) {
+
+        int[] arr = {167, -2, 16, 99, 26, 92, 43, -234, 35, 80};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
