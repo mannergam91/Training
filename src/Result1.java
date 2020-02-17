95cfbd7477cb4213f2f8f6e1d1a32edb2998efa9
MMakhov <mahovme@gmail.com>
2020-02-17 10:16:25 +0300

    11

--- src/Result1.java
+++ src/Result1.java
@@ -5,7 +5,6 @@ public class Result1 {
         Scanner in = new Scanner(System.in);
         int d = in.nextInt();
         StringBuilder sb = new StringBuilder();
-
         int x = 1;
         for (int i = 0; i < 32; i++) {
             sb.append((d & x) == 0 ? "0" : "1");
